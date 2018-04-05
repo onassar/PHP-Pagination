81ca8e19530129d36e567204abe7175dcaf1522b
nanacudo <rg.edgard@gmail.com>
2018-04-05 11:10:28 -0600

    creación de ejemplo 1

---
+++ examples/sample-1.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Decripción Corta del archivo
+ * 
+ * Descripción larga (explicar próposito)...
+ * 
+ * IMPORTANTE:
+ * Este programa es propiedad de Pagadito El Salvador S.A. de C.V. se prohibe su 
+ * uso no autorizado, asi como cualquier alteración ó agregado sin previa 
+ * autorización.
+ * 
+ * @author Edgard Rodas <e.rodas@pagadito.com>
+ * @copyright Copyright (c) 2018 Pagadito El Salvador S.A. de C.V.
+ * 
+ */
+
+require_once '../Pagination.class.php';
+
+// determine page (based on <_GET>)
+$page = isset($_GET['page']) ? ((int) $_GET['page']) : 1;
+
+// instantiate; set current page; set number of records
+$pagination = (new Pagination());
+$pagination->setCurrent($page);
+$pagination->setTotal(200);
+
+// grab rendered/parsed pagination markup
+$markup = $pagination->parse();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP-Pagination Demo</title>
+        <link href="../themes/light.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>PHP-Pagination Demo</h1>
+        <div class="pagination"><?php echo $markup; ?></div>
+    </body>
+</html>
